bca1d2e47cb2d116cfb29ffd723546805330d828
LeonidScorp <fishchukl28@gmail.com>
2021-04-07 15:48:14 +0300

    maybe finished

--- js/main.js
+++ js/main.js
@@ -1,62 +1,14 @@
-// var coursesChildren = $(".course-slider-container").children().length;
-// var dotsVisible=true;
-// if (coursesChildren < 4) {
-//     dotsVisible = false;
-//     if (coursesChildren < 3) {
-//         // $(".course-slider-container").removeClass("course-slider");
-//         $(".course-slider-container").addClass("flex-container");
-//     }
-        
-// } else coursesChildren = 3;
-
-// $('.course-slider').slick({
-//     dots: dotsVisible,
-//     arrows:false,
-//     infinite: false,
-//     speed: 300,
-//     slidesToShow: coursesChildren,
-//     slidesToScroll: 1,
-//     responsive: [
-//         {
-//         breakpoint: 850,
-//         settings: {
-//             slidesToShow: 2,
-//             slidesToScroll: 1,
-//             dots: true
-//         }
-//         },
-//         {
-//         breakpoint: 600,
-//         settings: {
-//             slidesToShow: 1,
-//             slidesToScroll: 1,
-//             dots: true
-//         }
-//         }
-        
-//         // You can unslick at a given breakpoint now by adding:
-//         // settings: "unslick"
-//         // instead of a settings object
-//     ]
-// }); 
-
-// $(window).resize(function () {
-//     if (($(window).width() < 1220)) {
-//         dotsVisible = true;
-//     } else {
-//         if (coursesChildren < 4) dotsVisible = false;
-//     }
-// });
-
-var sections = $('section'), nav = $('.main-menu'), nav_height = $('header').outerHeight();
+var sections = $('section'), nav = $('.navigation'), nav_height = $('header').outerHeight();
 
 $(window).on('scroll', function () {
     var wintop = $('html').offset().top;
     var cur_pos = $(this).scrollTop();
     if (cur_pos>wintop) {
-        $('header').css({'background-color': '#14141A', "transition-property": "background-color", "transition-duration": ".5s"});
+        $('header').addClass("header-bg");
     } else {
-        $('header').css({"background-color": "transparent", "transition-property": "background-color", "transition-duration": ".5s"});
+        if (!$(".main-menu-mobile").hasClass("shown")) {
+            $('header').removeClass("header-bg");
+        }
     }
     sections.each(function() {
        var top = $(this).offset().top - nav_height,
@@ -69,47 +21,51 @@ $(window).on('scroll', function () {
 });
 
 nav.find('a').on('click', function () {
-var $el = $(this), id = $el.attr('href');
-    
-    $('html, body').animate({
-        scrollTop: $(id).offset().top - nav_height
-    }, 500);
-    
-    return false;
-    });
+var $el = $(this), id = $el.attr('href');    
+$('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+}, 500);
+return false;
+});
 
-    // $('.feedbacks-slider').slick({
-    //     autoplay:true,
-    //     autoplaySpeed: 10000,
-    //     dots: true,
-    //     infinite: true,
-    //     arrows:false,
-    //     speed: 500,
-    //     fade: true,
-    //     cssEase: 'linear'
-    // });
-
-    $('.slider-container').slick({
-        autoplay:false,
-        autoplaySpeed: 10000,
-        dots: true,
-        infinite: true,
-        arrows:false,
-        speed: 500,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        responsive: [
-            {
-            breakpoint: 1250,
-            settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: true
-            }
+$('.slider-container').slick({
+    autoplay:false,
+    autoplaySpeed: 10000,
+    dots: true,
+    infinite: true,
+    arrows:false,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+        {
+        breakpoint: 1250,
+        settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true
         }
-        ]
-    });
+    }
+    ]
+});
 
-    
+function showMenu () {
+    var wintop = $('html').offset().top;
+    var cur_pos = $(this).scrollTop();
+    if (wintop==cur_pos) {
+        $('header').toggleClass("header-bg");
+    }
+    $(".main-menu-mobile").toggleClass("shown");
+}
 
-    
+$(".menu-link").click(function () {
+    $(".main-menu-mobile").removeClass("shown");
+});
+
+$('.navbar-toggle').click(function(event){
+    event.stopPropagation();
+});
+
+$(window).click(function () {
+    $(".main-menu-mobile").removeClass("shown");
+});
